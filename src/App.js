485f3a6fc9aa1667a184e282e410ab1bc9eada10
RatanNarayanHegde <ratanhegde08@gmail.com>
2023-01-07 16:32:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const Heading = ({ text }) => <h1>{text}</h1>;
+
+const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
+
+  return all !== 0 ? (
+    <div>
+      <h2>Statistics</h2>
+      <p> good : {good}</p>
+      <p>neutral : {neutral}</p>
+      <p>bad : {bad}</p>
+      <p>all : {all}</p>
+      <p>average : {average}</p>
+      <p>positive : {positive} %</p>
+    </div>
+  ) : (
+    <p>No feedback given</p>
+  );
+};
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading text="Give feedback" />
+      <div>
+        <Button text="good" onClick={() => setGood(good + 1)} />
+        <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
+        <Button text="bad" onClick={() => setBad(bad + 1)} />
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
-}
+};
 
 export default App;
